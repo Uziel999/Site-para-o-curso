374f5f909b44662e5bbb700972f31f4c96fd29e6
Uziel <inaciouziel@gmail.com>
2022-05-31 19:57:18 -0300

    Migrando para js

---
+++ SiteCurso2/script.js
@@ -0,0 +1,15 @@
+// function recebeDados() {
+//     let name = document.;
+//     let email = document.getElementById('#email');
+
+//         if (name || email == '') {
+//         alert('Está faltando dados..')
+//     } else {
+//         alert('tudo ok')
+//     }
+//     alert('ok')
+// }
+// function gravarDados(recebeDados){
+//     let resposta = document.querySelectorById('exibeNaTela');
+//     resposta += recebeDados();
+// }
--- formulario.php
+++ formulario.php
@@ -5,27 +5,17 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
-    <style>
-        body {
-            padding: 3%;
-        }
-
-        .titulo h1 {
-            text-align: center;
-        }
- 
-    </style>
+    <link rel="stylesheet" href="form.css">
     <title>Formulário</title>
 </head>
 
 <body>
-    <div class="titulo">
+    <header>
+        <div class="titulo">
         <h1>Formulário</h1>
-    </div>
-
-    <section>
+        </div>
+    </header>
+     <section>
         <form action="validacao.php" method="post">
 
             <div class="mb-3">
@@ -40,13 +30,16 @@
                 <label for="exampleFormControlTextarea1" class="form-label">Deixe sua duvida</label>
                 <textarea class="form-control" name="duvida" id="exampleFormControlTextarea1" rows="3" placeholder="Digite aqui..."></textarea>
             </div>
-            <button type="submit" class="btn btn-primary">Enviar</button>
+            <div class="bnt">
+                         <button type="submit" class="btn btn-primary">Enviar</button>
             <button type="reset" class="btn btn-success">Limapr </button>
             <a href="/SiteCurso/index.php" class="btn btn-success">Voltar</a>
+            </div>
+   
 
 
         </form>
-    </section>
+        </section>
 
 </body>
 
